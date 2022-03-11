57498f6e8bc2e68c16acc927b9acd887b8688bc8
devnowk <57873373+devnowk@users.noreply.github.com>
2022-03-11 14:35:58 +0900

    깊이 우선 탐색

--- it 취업을 위한 알고리즘 문제풀이 (with CC++)/84@. 휴가(DFS 활용).cpp
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)/84@. 휴가(DFS 활용).cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <vector>
+#include <iostream>
 
 std::vector<std::pair<int, int> > schedule(16); // 입력 : 상담 스케쥴
 int maxVal = 0; // 최대 수익 기록
@@ -8,19 +9,22 @@ int n; // 입력 : 여행가는 전날
 void DFS(int d, int sum) // d번째 날 누적 수입은 sum
 {   
     //printf("DFS(%d, %d)\n", d, sum);
-    if(d > n) // n번째 날 넘으면 끝
+    if(d == n+1) // n+1일에 종료됨
     {
         if(sum > maxVal) maxVal = sum;
         return;
-    }
+    } else if(d>n+1) return; // 날짜가 넘어가면 하면 안됨
+    
     std::pair<int, int> tmp = schedule[d]; // d번째 날 스케쥴
-    if(d+tmp.first <= n+1) // 마지막 날이 1이면 할 수 있음(시작을 0부터 했어야..)
-        DFS(d+tmp.first, sum+tmp.second); // d번째 날에 상담할 경우
+    
+    // if(d+tmp.first <= n+1) 조건을 걸어서 미리 넘어가지 못하게 방지 가능
+    DFS(d+tmp.first, sum+tmp.second); // d번째 날에 상담할 경우
     DFS(d+1, sum); // d번째 날에 상담하지 않을 경우, 그 다음 날로 넘어감    
 }
 
 int main()
 {
+    ios_base::sync_with_stdio(false); // @@@ iostream 입출력 속도 빠르게 하기 @@@
     // freopen("파일이름", "rt", stdin);
     
     scanf("%d", &n);
