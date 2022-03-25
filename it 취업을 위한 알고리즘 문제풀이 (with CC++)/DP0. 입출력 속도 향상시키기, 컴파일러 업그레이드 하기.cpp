4d37974a395e1c7ddafa0cab7e628925ab427abe
devnowk <57873373+devnowk@users.noreply.github.com>
2022-03-25 23:53:14 +0900

    입출력 속도 향상 시키기 - ios_base::sync_with_stdio(false)

--- it 취업을 위한 알고리즘 문제풀이 (with CC++)/DP0. 입출력 속도 향상시키기, 컴파일러 업그레이드 하기.cpp
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)/DP0. 입출력 속도 향상시키기, 컴파일러 업그레이드 하기.cpp
@@ -1,11 +1,12 @@
 // #include <iostream>
-#include<bits/stdc++.h> // 모든 스탠다드 라이브러리 파일 포함 c++14, 컴파일 타임이 늘어남
+#include<bits/stdc++.h> // 모든 스탠다드 라이브러리 파일 포함 c++14(GCC컴파일러), 컴파일 타임이 늘어남
+// 백준 문제(+보통 채점 사이트)는 런타임 시간을 채점함. 위 헤더 파일은 컴파일 타임에 일어나므로 상관 X
 
 int main()
 {
     // 입력과 출력을 사용할 때, 원래 동기화를 함(cin, cout 표준 스트림 버퍼와 printf, scanf 표준 스트림 버퍼를 병행해서 사용)
-    ios_base::sync_with_stdio(false); // 동기화를 해제시켜서, c++표준 스트림만 사용함, 훨씬 빨라짐
-    cin.tie(NULL); // cin, cout 사용하는 버퍼 묶음을 해제시켜 조금만 더 빠르게 만든다.
+    std::ios_base::sync_with_stdio(false); // 동기화를 해제시켜서, c++표준 스트림만 사용함, 훨씬 빨라짐
+    std::cin.tie(NULL); // cin, cout 사용하는 버퍼 묶음을 해제시켜 조금만 더 빠르게 만든다.
     
     int n, min = 2147000000;
     std::cin >> n;
