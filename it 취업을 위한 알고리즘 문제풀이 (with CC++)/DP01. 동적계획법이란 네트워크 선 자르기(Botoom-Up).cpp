cd403f3f73dfb90276360e4259274dd6ec10c7d7
devnowk <57873373+devnowk@users.noreply.github.com>
2022-03-26 23:48:59 +0900

    동적계획법

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)/DP01. 동적계획법이란 네트워크 선 자르기(Botoom-Up).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    
+    // 동적 계획법 : 아주 복잡한 문제를 직관적으로 풀 수 있을 정도의 문제로 작게 분해해서 해를 저장해둠
+    //               조금 더 큰 문제의 해를 구해서 이전에 구했던 해와 관계식을 찾음
+    //               최종 점화식을 구해서 문제의 답을 찾음 (Bottom-Up)
+    
+    
+    
+    
+    return 0;
+}
