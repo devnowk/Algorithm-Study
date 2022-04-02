b9910bbcdf2d1d3588e13456f331524ca424a5e5
devnowk <57873373+devnowk@users.noreply.github.com>
2022-04-02 23:33:36 +0900

    동적 계획법

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)/DP07. 알리바바와 40인의 도둑(Bottom-Up).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    
+    // dp : dy[i][j] : (0,0)->(i,j)로 가는 데 드는 최소 비용
+    
+    
+    return 0;
+}
