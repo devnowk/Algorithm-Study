3c60a5c8afd8a978b0f2c65525ae49c539462874
devnowk <57873373+devnowk@users.noreply.github.com>
2022-04-21 22:26:02 +0900

    수학, DP, 조합론, DFS

---
+++ Baekjoon Online Judge/수학/실5_1010. 다리 놓기.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+int dp[30][30]; // 이항계수 메모이제이션 배열
+int DFS(int n, int r)
+{
+    if(dp[n][r]!=0) return dp[n][r];
+    if(n==r) return 1;
+    if(r==1) return n;
+    return dp[n][r] = DFS(n-1, r-1)+DFS(n-1, r);
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    
+    int t;
+    std::cin >> t;
+    for(int i=0; i<t; i++)
+    {
+        int n, m; // 입력 : 서쪽과 동쪽의 개수(0<n<=m<30)
+        std::cin >> n >> m;
+        // 최대한 다리를 지으려면 동쪽 개수 중 서쪽 개수를 선택한 후 서쪽 다리를 배치하면 됨(mCn 구하기)
+        std::cout << DFS(m, n) << "\n";
+    }
+    
+    return 0;
+}
