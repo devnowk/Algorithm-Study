44da298d592bad562ab901876e79c130867d6ece
devnowk <57873373+devnowk@users.noreply.github.com>
2022-04-26 23:56:26 +0900

    백트래킹, DFS

---
+++ Baekjoon Online Judge/백트래킹/실3_15652. N과 M(4).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int n, m; // 입력 : 1~n 중 m개를 고르는 수열 구하기(깊이 우선으로 탐색)(1<=m<=n<=8)
+int arr[9]; // 1부터 m까지 들어 있는 수를 출력
+bool ch[9]; // 1부터 8까지 방문했는지 체크
+void DFS(int idx)
+{
+    if(idx>m) // 선택된 개수가 m이면 리턴
+    {
+        for(int i=1; i<=m; i++) std::cout << arr[i] << " ";
+        std::cout << "\n";
+        return;
+    }
+    for(int i=arr[idx-1]; i<=n; i++) // 이전 값 부터 돌려야 비내림차순
+    {
+        arr[idx]=i; // idx번째 숫자 정함
+        DFS(idx+1); // 다음 숫자 정함
+    }
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    
+    std::cin >> n >> m;
+    arr[0]=1; // 이전 값 참조 때, 1부터 돌려야 하므로 1로 초기화
+    DFS(1); // 첫 번째 수부터 정하기
+    
+    return 0;
+}
