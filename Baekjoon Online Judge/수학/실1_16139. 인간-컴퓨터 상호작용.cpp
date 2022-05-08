d7a5e05cc302a0b160cb11c5c62e1ded0a0791fc
devnowk <57873373+devnowk@users.noreply.github.com>
2022-05-08 23:18:14 +0900

    [부분성공] 수학, 문자의 누적 합

--- Baekjoon Online Judge/수학/실1_16139. 인간-컴퓨터 상호작용.cpp
+++ Baekjoon Online Judge/수학/실1_16139. 인간-컴퓨터 상호작용.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 
-int sum[26][2001]; // sum[i][j] : i번째 문자(i : asckii code-97)의 문자열 j번째까지 누적 개수
+int sum[26][200001]; // sum[i][j] : i번째 문자(i : asckii code-97)의 문자열 j번째까지 누적 개수
 int main()
 {
     std::ios_base::sync_with_stdio(false);
@@ -9,21 +9,21 @@ int main()
     int q; // 입력 : 질문의 수(q<=2,000)
     std::cin >> str >> q;
     
-    // 어떻게 이전 값에 접근하는지 방법 -> 계속 같은 값을 넣어놓는다
-        // 이전 값이 존재할 때 복사
-    for(int i=1; i<=str.length(); i++)
+    for(unsigned int i=1; i<=str.length(); i++) // i-1번째 문자의 누적 개수 저장
     {
-        int idx=str[i]-97; // a~z => 0~25
-        for(int j=i-1; j>=0; j++)
+        int idx=str[i-1]-97; // a~z => 0~25
+        for(int j=0; j<26; j++) // 모든 알파벳 행 다 검사
         {
-            if(sum[idx][j]>0)
-                sum[idx][i]=sum[idx][i-1]+1; // 이전 값에서 하나 더해야 누적 개수가 됨
+            if(j==idx) sum[j][i]=sum[j][i-1]+1; // 해당 알파벳에 +1
+            else sum[j][i]=sum[j][i-1]; // 나머지는 전 값과 동일
         }
     }
     for(int i=0; i<q; i++)
     {
         char a; // 입력 : 찾는 문자
         int l, r; // 입력 : 찾을 구간
+        std::cin >> a >> l >> r;
+        std::cout << sum[a-97][r+1]-sum[a-97][l] << "\n"; // 누적 값이므로 5번째에서 0번째 빼면 1~5 사이 누적 값 나옴
     }
 
 
