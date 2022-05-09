9ebf664ffb1475ebf1cbf867751adc71608582e8
devnowk <57873373+devnowk@users.noreply.github.com>
2022-05-10 00:40:39 +0900

    Update 실4_10815. 숫자 카드.cpp
    이분 검색, 숫자 찾기

--- Baekjoon Online Judge/실4_10815. 숫자 카드.cpp
+++ Baekjoon Online Judge/실4_10815. 숫자 카드.cpp
@@ -1,26 +1,35 @@
 #include<iostream>
-#include<map>
+#include<algorithm>
 
 int main()
 {
     std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);std::cout.tie(NULL);
     
-    std::map<int, bool> map; // 입력 : 상근이 숫자 카드
-    int n; // 입력 : 상근이가 가진 카드 개수(1<=n<=500,000)
+    int n, m; // 입력 : 가진 카드 개수, 확인할 카드 개수(1<=n, m<=500,000)
     std::cin >> n;
-    for(int i=0; i<n; i++) 
-    {
-        int tmp;
-        std::cin >> tmp;
-        map[tmp]=true;
-    }
-    int m; // 입력 : 확인할 카드 개수
+    int card[500001];
+    for(int i=0; i<n; i++) std::cin >> card[i];
+    std::sort(card, card+n);
     std::cin >> m;
-    for(int i=0; i<m; i++)
+    for(int i=0; i<m; i++) // 이분 검색을 통해 카드가 있는지 확인
     {
-        int tmp;
-        std::cin >> tmp;
-        if(map[tmp]) std::cout << 1 << " ";
+        int target; // 찾을 카드
+        std::cin >> target;
+        int lt=0, rt=n-1;
+        bool isExist=false;
+        while(lt<=rt)
+        {
+            int mid=(lt+rt)/2;
+            if(card[mid]<target) lt=mid+1;
+            else if(target<card[mid]) rt=mid-1;
+            else if(target==card[mid]) 
+            {
+                isExist=true;
+                break;
+            }
+        }
+        if(isExist) std::cout << 1 << " ";
         else std::cout << 0 << " ";
     }
 
