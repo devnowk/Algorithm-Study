99624f1f5e584392714a8ada9d2ff4cb5fd1ba62
devnowk <57873373+devnowk@users.noreply.github.com>
2022-05-23 23:51:08 +0900

    순열 함수(next_permutation)

--- Programmers 코딩테스트 연습/Lv2. 단체사진 찍기.cpp
+++ Programmers 코딩테스트 연습/Lv2. 단체사진 찍기.cpp
@@ -1,16 +1,13 @@
-﻿#include <string>
-#include <vector>
-#include <iostream>
-#include <cmath>
+﻿#include <vector>
+#include <string>
+#include <algorithm>
 #include <unordered_map>
 
 using namespace std;
 
-unordered_map<char, int> arr; // 순열 맵(이름과 인덱스 저장)
-vector<char> names(8); // 프렌즈 이름
-vector<bool> visit(8); // A,C,F,J,M,N,R,T 방문 체크
+unordered_map<char, int> name; // 프렌즈 이름
+vector<int> arr(8); // 인덱스는 각 프렌즈 이름을 가리키고 값은 위치를 가리킴
 vector<string> data_copy; // 입력받은 조건들
-int answer; // 조건에 맞는 경우의 수
 
 bool Condition() // 입력받은 조건에 맞는지 확인
 {
@@ -20,57 +17,30 @@ bool Condition() // 입력받은 조건에 맞는지 확인
         string tmp = data_copy[i];
         char p1 = tmp[0], p2 = tmp[2]; // 거리를 구할 두 사람 이름
         char oper = tmp[3]; // 연산자
-        int dis = (tmp[4]-48) + 1; // 거리조건(원래 조건은 사이의 사람 수를 센 것이므로 거리는 +1 해줘야 함)
-        int real_dis = abs(arr[p1] - arr[p2]);
+        int dis = (tmp[4] - 48) + 1; // 거리조건(원래 조건은 사이의 사람 수를 센 것이므로 거리는 +1 해줘야 함)
+        int real_dis = abs(arr[name[p1]] - arr[name[p2]]);
 
-        switch (oper)
-        {
-        case '=':
-            if (real_dis != dis) check = false;
-            break;
-        case '>':
-            if (real_dis <= dis) check = false;
-            break;
-        case '<':
-            if (real_dis >= dis) check = false;
-            break;
-        }
+        if (oper == '=' && real_dis != dis) check = false;
+        else if (oper == '>' && real_dis <= dis) check = false;
+        else if (oper == '<' && real_dis >= dis) check = false;
     }
     return check;
 }
 
-void DFS(int idx)
-{
-    if (idx == 8)
-    {
-        //for (int i = 0; i < 8; i++) cout << arr[i] << " ";
-        //cout << "\n";
-        if (Condition()) answer++; // 현재 순열(arr)이 조건에 맞으면 ++
-
-        return;
-    }
-    // 중복되지 않는 순열 구하기
-    for (int i = 0; i < 8; i++)
-    {
-        if (!visit[i]) // 방문한 적이 없을 때 순열에 넣을 수 있음
-        {
-            arr[names[i]] = idx;
-            visit[i] = true;
-            DFS(idx + 1);
-            visit[i] = false; // 방문 후에는 체크 해제
-        }
-    }
-}
 
 int solution(int n, vector<string> data) {
-    // 전역변수 초기화
-    answer = 0;
-    arr.clear();
-    names = { 'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T' };
-    visit.assign(20, false);
-    data_copy = data;
 
-    DFS(0); // 0번째 값 정하기
+    // 전역변수 초기화
+    data_copy = data;
+    for (int i = 0; i < 8; i++) arr[i] = i;
+    name = { {'A',0}, {'C',1}, {'F',2}, {'J',3}, {'M',4}, {'N',5}, {'R',6}, {'T',7} };
+
+    int answer = 0; // 조건에 맞는 경우의 수
+    do
+    {
+        if (Condition()) answer++;
+
+    } while (next_permutation(arr.begin(), arr.end()));
 
     return answer;
 }
