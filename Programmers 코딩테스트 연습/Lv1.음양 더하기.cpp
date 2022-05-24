ba2544a8dfd1e24c2f180acbc082497658c17aa3
devnowk <57873373+devnowk@users.noreply.github.com>
2022-05-24 16:45:44 +0900

    수학

---
+++ Programmers 코딩테스트 연습/Lv1.음양 더하기.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+
+    for (int i = 0; i < signs.size(); i++)
+    {
+        answer += signs[i] ? absolutes[i] : -absolutes[i];
+    }
+
+    return answer;
+}
+int main()
+{
+    //vector<string> tmp = { "N~F=0", "R~T>2" };
+    //vector<string> str = solution(tmp);
+    //for (int i = 0; i < str.size(); i++) std::cout << str[i] << "\n";
+    vector<int> num = { 4,7,12 };
+    vector<bool> s = { true,false,true };
+    //string tmp = "right";
+    //vector<vector<int> > tmp = { {1, 1, 1, 0},{1, 1, 1, 0},{0,0,0,1},{0, 0, 0, 1},{0, 0, 0, 1},{0, 0, 0, 1} };
+
+    cout << solution(num, s);
+    //cout << v[0] << " " << v[1];
+
+    return 0;
+}
